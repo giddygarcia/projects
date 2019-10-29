2c890b6fe9448bc2bc5c73240f726182d59d19f6
giddyg <christine.garcia@obf.ateneo.edu>
2019-10-29 20:23:25 +0800

    Transfer airport-logger to own repo

--- airport-logger/airport-final/app/models/airplane.rb
+++
@@ -1,7 +0,0 @@
-class Airplane < ApplicationRecord
-	validates :code, presence:true, length:{maximum: 20}
-	validates :seat_capacity, presence:true, length:{maximum: 4}
-	validates :destination, presence:true
-	validates :time_in, presence:true
-	validates :time_out, presence:true
-end
--- airport-logger/airport-final/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
-  resources :airplanes
-  get 'members', to: 'pages#members'
-  get 'home', to: 'pages#home'
-end
